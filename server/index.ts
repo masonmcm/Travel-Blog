a3a8954a9a3ea824b7b464bc4e4aea0a2dc83d74
Mason Mathew <masonmc@live.unc.edu>
2019-05-30 22:50:24 +0200

    Server side rendering working!

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as exphbs from "express-handlebars"
+import { reduceEachLeadingCommentRange } from "typescript";
 
 let app = express();
 
@@ -19,8 +20,29 @@ app.engine("hbs", exphbs({
 }));
 
 app.get("/", (req, res) => {
-    res.render("index");
-})
+    res.render("index", {
+        title: "On the Go!"
+    })
+});
+
+app.get("/itinerary", (req, res) => {
+    res.render("itinerary", {
+        title: "Itinerary"
+    })
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About"
+    })
+});
+
+app.get("/gallery", (req, res) => {
+    res.render("gallery", {
+        title: "Gallery"
+    })
+});
+
 
 app.listen(1234, () => console.log("Listening on 1234"))
     .on("error", (e) => console.error(e));
---
+++ server/views/script/Data.ts
@@ -0,0 +1,56 @@
+export default [
+    {
+        "thumbSrc": "./img/sketch-1.jpg",
+        "src": "./img/sketch-1.jpg",
+        "widthThumb": 128,
+        "heightThumb": 171,
+        "width": 3024,
+        "height": 4032,
+        "caption" : "A sketch done in the dormitory"
+    },
+    {
+        "thumbSrc": "./img/sketch-2.jpg",
+        "src": "./img/sketch-2.jpg",
+        "widthThumb": 128,
+        "heightThumb": 171,
+        "width": 3024,
+        "height": 4032,
+        "caption" : "A sketch done in Charles de Gaulle"
+    },
+    {
+        "thumbSrc": "./img/sketch-3.jpg",
+        "src": "./img/sketch-3.jpg",
+        "widthThumb": 128,
+        "heightThumb": 171,
+        "width": 3024,
+        "height": 4032,
+        "caption" : "A sketch done in Raleigh-Durham International"
+    },
+    {
+        "thumbSrc": "./img/world-thumb.jpg",
+        "src": "./img/world.jpg",
+        "widthThumb": 128,
+        "heightThumb": 171,
+        "width": 3024,
+        "height": 4032,
+        "caption" : "Mona Hatoum at ARoS"
+    },
+    {
+        "thumbSrc": "./img/decon-thumb.jpg",
+        "src": "./img/decon.jpg",
+        "widthThumb": 128,
+        "heightThumb": 171,
+        "width": 3024,
+        "height": 4032,
+        "caption" : "Arthur Kopcke at ARoS"
+    },
+    {
+        "thumbSrc": "./img/leaves-thumb.jpg",
+        "src": "./img/leaves.jpg",
+        "widthThumb": 128,
+        "heightThumb": 96,
+        "width": 4032,
+        "height": 3024,
+        "caption" : "An artist whose name I forgot at ARoS"
+    },
+];
